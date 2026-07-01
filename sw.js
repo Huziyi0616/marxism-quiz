b480efd4debd90f9a24f9dfce0c754b7fc97e9b6
Huziyi0616 <huziyi0616@qq.com>
2026-07-02 00:15:58 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "marxism-quiz-pwa-v3";
+const CACHE_NAME = "marxism-quiz-pwa-v4";
 const ASSETS = [
   "./",
   "index.html",
@@ -26,12 +26,12 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request).then(response => {
+    fetch(event.request).then(response => {
         const copy = response.clone();
         caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
         return response;
-      });
+      }).catch(() => {
+        return caches.match(event.request);
     })
   );
 });
